7bda1d0e3c1e39a4c25766522f899c902abb54b5
Khorshed Hasan Tutul <joymania_pust@outlook.com>
2021-04-01 22:45:31 +0600

    mastering page

---
+++ admin/sidebar.php
@@ -0,0 +1,28 @@
+ <!--sidebar start-->
+ <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+                  <li>
+                      <a class="active" href="index.html">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-laptop"></i>
+                          <span>Categories</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="add_category.php">Add Category</a></li>
+                          <li><a  href="manage_category.php">Manage Category</a></li>
+                         
+                      </ul>
+                  </li>
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
